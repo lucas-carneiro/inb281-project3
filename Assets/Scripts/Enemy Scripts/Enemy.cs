23a74c8743ac363e449b5227bd58e94753c4d672
lcarneiro <lucas.rod.carneiro@gmail.com>
2016-04-20 22:00:17 +1000

    Adding an enemy script to store HP status.

---
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public float maxHP = 100f;
+    public float currentHP;
+    public enum Status { walk, taunt, attack, hit, die };
+    private Status currentStatus;
+
+    // Use this for initialization
+    void Start () {
+        currentHP = maxHP;
+        currentStatus = Status.walk;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (currentStatus == Status.die) {
+            Destroy(gameObject);
+        }
+	}
+
+    public void TakeDamage(float damage) {
+        currentHP -= damage;
+        if (currentHP <= 0) {
+            currentStatus = Status.die;
+        }
+        else {
+            currentStatus = Status.hit;
+        }
+        //Play animation
+        //GetComponent<Animation>().Play(currentStatus.ToString());
+    }
+}
